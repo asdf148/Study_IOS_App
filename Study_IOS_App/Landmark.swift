ecbecaed095144f5b2649dc762a57cced0a7ef95
asdf148 <wnduf0405_@naver.com>
2022-02-14 17:19:03 +0900

    Building Lists and Navigation: [Section 1] Create a Landmark Model

---
+++ Study_IOS_App/Landmark.swift
@@ -0,0 +1,37 @@
+//
+//  Landmark.swift
+//  Study_IOS_App
+//
+//  Created by LeeJuYeol on 2022/02/14.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
