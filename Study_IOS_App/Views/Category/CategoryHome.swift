448c57cb8d97db8a78ab257cb77f660ea18b0e24
asdf148 <wnduf0405_@naver.com>
2022-02-16 15:02:20 +0900

    Composing Complex Interfaces: [Section 1] Add a Category View

---
+++ Study_IOS_App/Views/Category/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Study_IOS_App
+//
+//  Created by LeeJuYeol on 2022/02/16.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
